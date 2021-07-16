ca0f3d61edc401e01b51e7b440b4c85d1cb00349
quantosapplications <info@quantos.it>
2021-07-16 12:27:17 +0200

    depèloy

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-// export { RedisMapHelper, RedisMapHelperConfig } from './redis_map_helper';
-// export { RedisListHelper, RedisListHelperConfig } from './redis_list_helper';
-// export { RedisValueHelper, RedisValueHelperConfig } from './redis_value_helper';
+import microrequest from './micro_request';
+export default microrequest;
